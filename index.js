7fa49e7c82ae98a764e315ebef6e4b8072347cdf
Aisulu <aisulutulepova4@gmail.com>
2024-12-09 10:53:19 +0500

    baqylay

---
+++ index.js
@@ -0,0 +1,52 @@
+// 1-тапсырма:E-Shop Объектісін Құру
+eShop= {
+    name: 'BestShop',
+    phonNumber: 87056717400,
+    products:
+       [{   
+            productName: 'Syrga',
+            category: 'bijuteriya', 
+            price:3500,
+            stock:10,
+       },
+       
+       { 
+           productName: 'Alga',
+           category: 'bijuteriya', 
+           price:7000,
+           stock:15,
+       
+       },
+       
+       { 
+           productName: 'iphone',
+           category: 'elektronika', 
+           price:500000,
+           stock:12,
+       },
+       
+       { 
+           productName: 'utejok',
+           category: 'elektronika', 
+           price:17000,
+           stock:8,
+       
+       },
+       { 
+        productName: 'Sagat',
+        category: 'ukrashenie', 
+        price: 15000,
+        stock:5,
+       },
+    ]
+}
+
+// // 2-тапсырма:
+// let changeShopName = prompt('Жаңа атау енгізіңіз:')
+// eShop.name1=name
+// console.log(changeShopName);
+// console.log(eShop);
+
+// 3-тапсырма:
+let changePhonepName = prompt("телефон номер жаз:")
+eShop.phonNumber = phone
